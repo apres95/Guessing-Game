639a5881e245697ec236d2de39280d40cd4333b8
apres95 <alexisepresbrey@gmail.com>
2017-04-07 12:36:39 -0700

    commit

--- js/main.js
+++ js/main.js
@@ -4,11 +4,13 @@ function guessingGame() {
    var guess = document.getElementById("guessInput").value
   if (guess == answer) {
    $("p").prepend("Congratulations! <br>");
+   } else if (guess == "") {
+   $("p").prepend("Please enter a number. <br>");
    } else if (guess > answer) {
     $("p").prepend(guess + " is too high! Try Again! <br>");
    return
    } else if (guess < answer) {
     $("p").prepend(guess + " is too low! Try Again! <br>");
    return
-   }
+   } 
 }
